858f4fe54ee4c2773e5fbec27ec917cc66ad534a
Dennis Jensen <ngh1adj95@hotmail.com>
2024-03-07 19:59:38 +0100

    Feature: Blinky esp32c3

---
+++ flake.nix
@@ -0,0 +1,26 @@
+# flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [rust-overlay.overlays.default];
+    };
+    toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        toolchain
+      ];
+    };
+  };
+}
